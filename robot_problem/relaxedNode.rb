4f09a33bca735eed8aa234035a5f98011100ae2d
Diego Araújo <diegoamc90@gmail.com>
2015-05-20 19:35:51 -0300

    Noop-first and difficulty heuristics

--- robot_problem/relaxedNode.rb
+++ robot_problem/relaxedNode.rb
@@ -5,6 +5,15 @@ class RelaxedNode
     @predicate = predicate
     @depth = Float::INFINITY
     @sucessor = []
-    @parent = []
+    # Here we are using the Noop-first heuristic combined with the difficulty heuristic
+    @parent = PQueue.new([]) do |relaxed1, relaxed2|
+      if relaxed1.predicate.start_with?("No-op") && !relaxed2.predicate.start_with?("No-op")
+        1
+      elsif relaxed2.predicate.start_with?("No-op") && !relaxed1.predicate.start_with?("No-op")
+        -1
+      else
+        relaxed1.depth < relaxed2.depth
+      end
+    end
   end
 end
