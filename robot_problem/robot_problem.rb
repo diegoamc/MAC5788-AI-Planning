41ecd81c846dfced2941d3cdcbaf3f5bac6acc30
vibonadia <vivianebonadia@gmail.com>
2015-05-22 00:06:33 -0300

    heuristics type and domain parameterized

--- robot_problem/robot_problem.rb
+++ robot_problem/robot_problem.rb
@@ -1,21 +1,25 @@
 require_relative 'requirements'
 
-problem_test = ARGV[0]
-ground = ARGV[1]
+domain_name = ARGV[0] # domain.pddl | blocksWorldDomain.pddl | satelliteDomain.pddl
+problem_test = ARGV[1]
+heuristic = ARGV[2] # graphPlanHeuristic | heuristic0 | heuristic1
+ground = ARGV[3] # all
 
-#parse the PDDL input
-#domain_pddl = SExpr.new File.read("domain.pddl")
-domain_pddl = SExpr.new File.read("satelliteDomain.pddl")
-#domain_pddl = SExpr.new File.read("blocksWorldDomain.pddl")
-#problem_pddl = SExpr.new File.read("problems/" << problem_test)
-problem_pddl = SExpr.new File.read("problems/Satellites/" << problem_test)
-#problem_pddl = SExpr.new File.read("problems/BlocksWorld/" << problem_test)
+# run examples:
+# ruby robot_problem.rb robotDomain.pddl problem2Box2Room.pddl graphPlanHeuristic all
+# ruby robot_problem.rb robotDomain.pddl problem6Box2Room.pddl heuristic0 all
+# ruby robot_problem.rb blocksWorldDomain.pddl probBLOCKS-4-0.pddl graphPlanHeuristic all
+# ruby robot_problem.rb blocksWorldDomain.pddl probBLOCKS-4-0.pddl heuristic0 all
+
+# parse the PDDL input
+domain_pddl = SExpr.new File.read(domain_name)
+problem_pddl = SExpr.new File.read("problems/#{domain_name.gsub(".pddl", "")}/" << problem_test)
 
 problem = Problem.new problem_pddl.data.drop 1
 domain = Domain.new domain_pddl.data.drop 1
 
 start_time = Time.now
-node_solution = Search.a_star_tree_search(domain.grounded_actions, problem, domain, "graphPlanHeuristic", ground)
+node_solution = Search.a_star_tree_search(domain.grounded_actions, problem, domain, heuristic, ground)
 end_time = (Time.now - start_time) * 1000
 time = ("Time: %.2f" % end_time + "ms")
 
@@ -25,8 +29,6 @@ else
   result = Search.path_to(node_solution)
 end
 
-#out_file = File.new("results/Blocks/result_NoOp_#{problem_test.gsub(".pddl", "")}_#{ground}.txt", "w")
-#out_file = File.new("results/HeuristicFF/result_NoOp_#{problem_test.gsub(".pddl", "")}_#{ground}.txt", "w")
-out_file = File.new("results/Satellites/result_NoOp_#{problem_test.gsub(".pddl", "")}_#{ground}.txt", "w")
+out_file = File.new("results/#{domain_name.gsub(".pddl", "")}/#{problem_test.gsub(".pddl", "")}_#{heuristic}_#{ground}.txt", "w")
 out_file.puts("#{time}\n#{result}")
 out_file.close
