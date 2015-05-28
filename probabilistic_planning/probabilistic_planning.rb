701bc4ef4246fc9b784d8a5ce5fefe23dc99e968
Diego Araújo <diegoamc90@gmail.com>
2015-05-28 10:18:40 -0300

    Initial requirements

--- probabilistic_planning/probabilistic_planning.rb
+++ probabilistic_planning/probabilistic_planning.rb
@@ -1,1 +1,2 @@
 # main file
+require_relative 'requirements'
--- probabilistic_planning/requirements.rb
+++ probabilistic_planning/requirements.rb
@@ -1,1 +1,9 @@
 # requirements file
+require_relative 'parser'
+
+require_relative 'domain/action.rb'
+require_relative 'domain/problem_definition.rb'
+require_relative 'domain/state.rb'
+
+require_relative 'algorithms/lao.rb'
+require_relative 'algorithms/lrtdp.rb'
